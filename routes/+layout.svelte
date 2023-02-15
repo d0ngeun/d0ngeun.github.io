c52c80af912936a1c816b39f27ea8499689e5738
James Park <jamesdpark1@gmail.com>
2023-02-15 17:05:50 -0500

    Updates

---
+++ routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import Header from '$lib/header/header.svelte'; 
+</script>
+
+<div class="app">
+	<Header>
+		<a href="/"><h1 class:small={$page.url.pathname !== '/'}>d0ngeun</h1></a>
+	</Header>	
+	<main>
+		<slot />
+	</main>
+</div>
